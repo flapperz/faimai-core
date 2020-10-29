1428a50ce9dfcfebe803f252ced6552acdd734d2
Krit Cholapand <flap.atyz@gmail.com>
2020-10-30 04:30:52 +0700

    CORE: Add broadcasting base

---
+++ core/receiver.py
@@ -0,0 +1,15 @@
+# base from https://github.com/ninedraft/python-udp
+import socket
+import json
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+client.bind(("", 37020))
+while True:
+    data, addr = client.recvfrom(1024)
+    print("received message: {}".format(data.decode('utf-8')))
