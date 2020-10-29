6a003a42edfc6dcfcbc0c17ad05a9350812c3ed8
Krit Cholapand <flap.atyz@gmail.com>
2020-10-30 05:58:11 +0700

    CORE: Omit sender print

--- core/sender.py
+++ core/sender.py
@@ -3,6 +3,9 @@
 import socket
 import time
 import json
+from os import environ
+
+print('Start sender')
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
@@ -15,8 +18,12 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 # indefinitely when trying to receive data.
 server.settimeout(0.2)
 
-message = b"your very important message"
+uuid = environ["UUID"]
+
+message = { 'uuid': uuid, 'status': 'nofire' }
+messageByte = json.dumps(message).encode('utf-8')
+
 while True:
-    server.sendto(message, ("<broadcast>", 37020))
-    print("message sent!")
+    server.sendto(messageByte, ("<broadcast>", 37020))
+    # print("message sent!")
     time.sleep(1)
