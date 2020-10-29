2e40fa819d53a1c7a54506c24d1e5c0b699e6d5f
ttkuea <turtlekungz@gmail.com>
2020-10-29 23:41:10 +0700

    GPIO: Sample code

---
+++ gpio/sensor_data.py
@@ -0,0 +1,10 @@
+from gpiozero import LineSensor
+from signal import pause
+
+sensor = LineSensor(4)
+sensor.when_line = lambda: print('Line detected')
+sensor.when_no_line = lambda: print('No line detected')
+pause()
+
+def getSensorData(sensor):
+    return sensor.value
