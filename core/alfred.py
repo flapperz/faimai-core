52965f4a523fc19bd2b5809c470e482ddc317173
Krit Cholapand <flap.atyz@gmail.com>
2020-11-12 14:28:54 +0700

    Change and update receiver to ALFRED

--- core/alfred.py
+++ core/alfred.py
@@ -1,11 +1,14 @@
+# AppLication For Read Entered Data
 # base from https://github.com/ninedraft/python-udp
 import socket
 import json
 import time
 from os import environ
 
-print('Start receiver')
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+PORT = 37020
+print('Start A.L.F.R.E.D - MESSAGE LISTENER at :{}'.format(PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                       socket.IPPROTO_UDP)  # UDP
 
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
@@ -15,7 +18,7 @@ client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 member = dict()
 
-client.bind(("", 37020))
+client.bind(("", PORT))
 while True:
     data, addr = client.recvfrom(1024)
     message = json.loads(data)
@@ -23,9 +26,10 @@ while True:
 
     recvId = message["uuid"]
 
-    if recvId != uuid: 
-        print("[{}] received message from <{}> status <{}> on <{}>".format(uuid, message["uuid"], message["status"], time.asctime(time.localtime(recvTime))))
-        
+    if recvId != uuid:
+        print("[{}] received message from <{}> status <{}> on <{}>".format(
+            uuid, message["uuid"], message["status"], time.asctime(time.localtime(recvTime))))
+
         if recvId not in member:
             member[recvId] = addr
             print("new entry uuid <{}> with address <{}>".format(recvId, addr))
