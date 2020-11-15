bb7356e48adc1334fa31b55082440a82bc4aa3aa
Krit Cholapand <flap.atyz@gmail.com>
2020-11-15 16:07:08 +0700

    Change information

--- core/joker.py
+++ core/joker.py
@@ -7,26 +7,10 @@ import json
 # Sample blog post data similar to
 # https://ordina-jworks.github.io/frontend/2019/03/04/vue-with-typescript.html#4-how-to-write-your-first-component
 PORT = 8000
-get_data = [
-    {
-        'title': 'My first blogpost ever!',
-        'body': 'Lorem ipsum dolor sit amet.',
-        'author': 'Elke',
-        'date_ms': 1593607500000,  # 2020 July 1 8:45 AM Eastern
-    },
-    {
-        'title': 'Look I am blogging!',
-        'body': 'Hurray for me, this is my second post!',
-        'author': 'Elke',
-        'date_ms': 1593870300000,  # 2020 July 4 9:45 AM Eastern
-    },
-    {
-        'title': 'Another one?!',
-        'body': 'Another one!',
-        'author': 'Elke',
-        'date_ms': 1594419000000,  # 2020 July 10 18:10 Eastern
-    }
-]
+get_data = {
+    "location": "Building 3",
+    "Description": "Reuse from pop bus project"
+}
 
 
 class _RequestHandler(BaseHTTPRequestHandler):
