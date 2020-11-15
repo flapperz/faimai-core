bb4060002dddca31a632b3f19375be195096022c
Krit Cholapand <flap.atyz@gmail.com>
2020-11-16 03:49:50 +0700

    Update robin to be able to receive change

--- core/robin.py
+++ core/robin.py
@@ -2,15 +2,20 @@
 
 import socket
 import time
-from gpiozero import LineSensor
 # Run Only for BroadcastINg
 import json
 from os import environ
+from multiprocessing.connection import Listener
+from gpiozero import LineSensor
+sensor = LineSensor(4)
 
 print('Start R.O.B.I.N. - STATUS BROADCASTER')
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+address = ('localhost',6000)
+listener = Listener(address)
+conn = listener.accept()
 
 # Enable broadcasting mode
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -18,13 +23,14 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 # Set a timeout so the socket does not block
 # indefinitely when trying to receive data.
 server.settimeout(0.2)
-sensor = LineSensor(4)
 uuid = environ["UUID"]
 
+seq = 0
 
 while True:
-    message = {'uuid': uuid, 'isFire': sensor.value == 1}
+    if conn.recv() == "update":
+		seq += 1
+    message = {'uuid': uuid, 'isFire':1, 'seq':seq}
     messageByte = json.dumps(message).encode('utf-8')
     server.sendto(messageByte, ("112.116.44.15", 28795))
-    # print("message sent!")
     time.sleep(1)
